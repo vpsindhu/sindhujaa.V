838156f700bc0ac914850886112170f26acadad9
V.Sindhujaa <vpsindhu@users.noreply.github.com>
2016-07-10 12:09:35 +0530

    Create rec.cpp

---
+++ rec.cpp
@@ -0,0 +1,31 @@
+#include<iostream.h>
+#include<stdio.h>
+void main()
+{
+char name={"TREE"};
+int thickness;
+int branches;
+char color[];
+int angle;
+printf("enter the thickness of the tree");
+scanf("%d,&thickness of the tree");
+printf("enter the branches of the tree");
+scanf("%d,branches of the tree");
+printf("enter the color of the tree");
+scanf("%c,&color of the tree");
+printf("enter the angle of the tree");
+scanf("%d,&angle of the tree");
+if(thickness>23)
+{
+if (branches>20)
+{
+if(angle<=45)
+{
+printf("\n the angle of the tree is:");
+}
+printf("\n the branches of the tree is:");
+}
+printf("\n the thickness of the tree is:");
+}
+getch()
+}
