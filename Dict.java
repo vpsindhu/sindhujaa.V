3ba5789ef3006fcf110cdbcf60496c9758a9322a
V.Sindhujaa <vpsindhu@users.noreply.github.com>
2016-07-11 09:52:29 +0530

    Create Dict.java

---
+++ Dict.java
@@ -0,0 +1,26 @@
+import java.io.*;
+class Dict
+{
+string *a;
+comp()
+Scanner *c=new scanner(System.in);
+string d=*c.nextString();
+try{
+strcmp(*a,*p)
+{
+System.out.ln("the outpit is hello"+a)
+}
+catch
+{
+System.out.println("the input is discorrect.enter correct word");
+}
+class input
+{
+public static void main(string arg[])
+{
+dictionary r=new dictionary();
+r.*a="hello";
+r.comp();
+}
+
+    
